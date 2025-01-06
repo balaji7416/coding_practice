d87337c8850facde43ad96a1fafb92d74ec2ab6a
balaji7416 <balaji.nani12345@gmail.com>
2025-01-06 22:32:35 +0530

    decimal to binary,octal,hexadecimal

--- C/mixed/decimalTo_bin.c
+++ C/mixed/decimalTo_bin.c
@@ -1,66 +1,55 @@
 #include<stdio.h>
 #include<math.h>
-int bin(int num);
+void bin(int num);
 int main(){
 
-    int ch1,ch2 ;
+    int ch ;
     int num;
 
-    printf("chooce the format to enter a num :\n");
-    printf("1.binary\n");
-    printf("2.octal\n");
-    printf("3.decimal\n");
-    printf("4.hexa decimal\n");
-    scanf("%d",&ch1);
-    
-    printf("enter number :");
-
-    if(ch1==2)
-    scanf("%o",&num);
-    
-
-    else if(ch1==3)
+    printf("enter a number :");
     scanf("%d",&num);
 
-    else if(ch1==4)
-    scanf("%x",&num);
-
-    printf("enter the form you want to convert :");
+    printf("enter the form you want to convert :\n");
     printf("1.binary\n");
     printf("2.octal\n");
     printf("3.decimal\n");
     printf("4.hexa decimal\n");
-    scanf("%d",&ch2);
+    scanf("%d",&ch);
+    
+    if(ch==1)
+   bin(num);
 
-    if(ch1==2)
+    else if(ch==2)
     printf("%o",num);
     
 
-    else if(ch1==3)
+    else if(ch==3)
     printf("%d",num);
 
-    else if(ch1==4)
+    else if(ch==4)
     printf("%x",num);
 
-    else
-    printf("%d",bin(num));
+    
     
 }
 
-int bin(int num){
-        int tem[100];
-        int rem;
-        int len=0;
-        int ans=0;
-        for(int i=num; i!=0; i++){
-           rem=num%2;
-           tem[i-num] = rem;
-           num/=2;
-           len++;
-        }
-        for(int i=0; i<len; i++){
-            ans+=tem[len-i-1]*pow(10,len-i-1);
-        }
-        return ans;     
+void bin(int num){
+   
+       if(num == 0){
+        return;
+       }  
+
+       int ans[50];
+       int index=0;
+
+       while(num != 0){
+        ans[index++] = num%2;
+        num/=2;
+       }
+       for(int i=index-1;i>=0;i--){
+        printf("%d",ans[i]);
+       }
+    
+       
           
 }
